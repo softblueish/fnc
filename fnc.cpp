fc9e204aaafd926b3982ae6d5268acfd99b84159
ph3b3 <phoebeleong8@gmail.com>
2021-09-15 17:17:03 +1000

    removed unneccesary comments

--- fnc.cpp
+++ fnc.cpp
@@ -1,8 +1,6 @@
-// Headers
 #include <iostream>
 #include <fstream>
 
-// Main program
 int main(int argc, char **argv){
 	
 	// Creates output for getline()
@@ -13,14 +11,12 @@ int main(int argc, char **argv){
 
 		// Opens a file
 		std::fstream file(argv[i]);
-	
-		// While loop that checks if the file has ended and if a file has been opened
+
 		while(!file.eof()&&file.is_open()){
 		
 			// Reads line and outputs it to the variable outputStr
 			getline(file, outputStr);
 
-			// Prints outputStr
 			std::cout << outputStr << std::endl;
 		}
 	};
