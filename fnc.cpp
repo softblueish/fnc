19ec8dc9cb6f651c5d8ea9464a16014d8557294a
ph3b3 <phoebeleong8@gmail.com>
2021-10-24 18:18:49 +1100

    added error msg on no files

--- fnc.cpp
+++ fnc.cpp
@@ -5,14 +5,18 @@
 // Main program
 int main(int argc, char **argv){
 	
+	if (argc == 1){
+	        std::cout << "Arguments must be supplied!\n";
+        	std::cout << "Usage: " << argv[0] << " <file1> <file2> <file3...\n";
+	        return -1;
+    	}
 	// Creates output for getline()
 	std::string outputStr;
-	std::fstream file;
 	// For loop to open files in order
 	for(int i = 1; i <= argc; i++){ 
 
 		// Opens a file
-        std::ifstream file(argv[i]);
+        	std::ifstream file(argv[i]);
 	
 		// While loop that checks if the file has ended and if a file has been opened
 		while(!file.eof()&&file.is_open()){
